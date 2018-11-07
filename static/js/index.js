8ff2593a593ae14bd27079715aac575ba0e7449a
Gregg Nurick <greggkn872@gmail.com>
2018-11-07 13:17:58 +0000

    awkwarrd

--- static/js/index.js
+++ static/js/index.js
@@ -2,7 +2,10 @@ var buttons = document.getElementsByClassName("btn");
 var cursor = document.getElementById("cursor");
 var data = document.getElementById("data");
 var model = document.getElementById("model");
-var prediction = document.getElementById("predictions")
+var prediction = document.getElementById("predictions");
+var toanalysis = document.getElementById("analysisbtn");
+var screen1 = document.getElementById("right");
+var screen2 = document.getElementById("right2");
 var clicked = 0;
 var page = 0;
 
@@ -11,6 +14,18 @@ hidelayer(clicked);
 
 for (let button of buttons){
     button.addEventListener("click", movecursor);
+}
+
+toanalysis.addEventListener("click", changescreen);
+
+function changescreen(){
+    if(page == 0){
+        page = 1
+    }
+    if(page == 1){
+        page = 0
+    }
+    swap(page);
 }
 
 function movecursor(e){
@@ -30,6 +45,11 @@ function movecursor(e){
     }
     
     hidelayer(clicked);
+}
+
+function swap(page){
+    screen1.style.visibility = (page == 1)?"hidden": "visible";
+    screen2.style.visibility = (page == 0)?"hidden": "visible";
 }
 
 function hidelayer(clicked){
