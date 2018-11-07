ddaceec3496c6dda8eddb5b7092cdb9b61a2841c
liamdalg <liamdalg99@gmail.com>
2018-11-07 11:02:18 +0000

    basic template rendering

--- server.py
+++ server.py
@@ -1,7 +1,15 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return 'Hello world!'
+
+@app.route('/tool')
+def tool():
+    return render_template('tool.html')
+
+if __name__ == "__main__":
+    app.run()
