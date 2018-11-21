f9fae092d71e4bea375cd576576d6f6e76a75d4e
Gregg Nurick <greggkn872@gmail.com>
2018-11-21 11:33:19 +0000

    added header

--- static/js/index.js
+++ static/js/index.js
@@ -11,50 +11,36 @@ var page = 0;
 
 hidelayer(clicked);
 
-
-for (let button of buttons){
-    button.addEventListener("click", movecursor);
+for (let button of buttons) {
+  button.addEventListener("click", movecursor);
 }
 
-toanalysis.addEventListener("click", changescreen);
+function movecursor(e) {
+  var name = e.target.innerHTML;
+  if (name === "DATA") {
+    // cursor.style.top = "0";
+    clicked = 0;
+  }
+  if (name === "MODEL") {
+    // cursor.style.top = "33%";
+    clicked = 1;
+  }
+  if (name === "PREDICTION") {
+    // cursor.style.top = "67%";
+    clicked = 2;
+  }
 
-function changescreen(){
-    if(page == 0){
-        page = 1
-    }
-    if(page == 1){
-        page = 0
-    }
-    swap(page);
+  hidelayer(clicked);
 }
 
-function movecursor(e){
-    var name = e.target.innerHTML;
-    if(name === "DATA"){
-        
-        cursor.style.top = "0";
-        clicked = 0;
-    }
-    if(name === "MODEL"){
-        cursor.style.top = "33%";
-        clicked = 1;
-    }
-    if(name === "PREDICTION"){
-        cursor.style.top = "67%";
-        clicked = 2;
-    }
-    
-    hidelayer(clicked);
+function swap(page) {
+  screen1.style.visibility = page == 1 ? "hidden" : "visible";
+  screen2.style.visibility = page == 0 ? "hidden" : "visible";
 }
 
-function swap(page){
-    screen1.style.visibility = (page == 1)?"hidden": "visible";
-    screen2.style.visibility = (page == 0)?"hidden": "visible";
-}
-
-function hidelayer(clicked){
-    data.style.visibility = (clicked == 1 || clicked == 2)?"hidden": "visible";
-    model.style.visibility = (clicked == 0 || clicked == 2)?"hidden": "visible";
-    prediction.style.visibility = (clicked == 0 || clicked == 1)?"hidden": "visible";
-    
+function hidelayer(clicked) {
+  data.style.visibility = clicked == 1 || clicked == 2 ? "hidden" : "visible";
+  model.style.visibility = clicked == 0 || clicked == 2 ? "hidden" : "visible";
+  prediction.style.visibility =
+    clicked == 0 || clicked == 1 ? "hidden" : "visible";
 }
