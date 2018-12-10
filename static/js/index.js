8cad901dfb27ea73602580b061710e0af48bd502
Nenivar <nenivar@gmail.com>
2018-12-10 10:25:32 +0000

    cleaner

--- static/js/index.js
+++ static/js/index.js
@@ -1,53 +1,42 @@
-var buttons = document.getElementsByClassName("btn");
 var cursor = document.getElementById("cursor");
-var data = document.getElementById("data");
-var model = document.getElementById("model");
-var prediction = document.getElementById("predictions");
 var toanalysis = document.getElementById("analysisbtn");
 var screen1 = document.getElementById("right");
 var screen2 = document.getElementById("right2");
 
+var data = document.getElementById("data");
+var model = document.getElementById("model");
+var prediction = document.getElementById("predictions");
 var newData = document.getElementById("displaycont__new");
 
 var clicked = 0;
-var page = 0;
 
 window.onload = function() {
+  var buttons = document.getElementsByClassName("btn");
+  for (let button of buttons)
+    button.addEventListener("click", movecursor);
+  
   displayLayer('data')
 }
 
+/*
+ *  LAYER SWITCHING FUNC.
+*/
+var layer = '';
 var LayerEnum = Object.freeze(
   {'data' : data,
    'model' : model,
    'prediction' : prediction,
    'newData' : newData}
 )
-
 // hide all layers apart from one w/ id
 function displayLayer(id) {
-  for (x in LayerEnum) {
+  for (x in LayerEnum)
     LayerEnum[x].style.visibility = x == id ? 'visible' : 'hidden';
-  }
+  layer = id
 }
-
-for (let button of buttons) {
-  button.addEventListener("click", movecursor);
-}
-
+// switch on button press
 function movecursor(e) {
-  var name = e.target.innerHTML;
-  switch(name) {
-    case 'DATA':
-      displayLayer('data')
-      break;
-    case 'MODEL':
-      displayLayer('model')
-      break;
-    case 'PREDICTION':
-      displayLayer('prediction')
-      break;
-    case 'dunno':
-      displayLayer('newData')
-      break;
-  }
+  var name = e.target.innerHTML.toLowerCase();
+  if (name in LayerEnum)
+    displayLayer(name)
 }
