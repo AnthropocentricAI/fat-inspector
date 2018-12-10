5db394aabb2f76456e6183221fe691f8ec71cf37
Nenivar <nenivar@gmail.com>
2018-12-10 13:14:56 +0000

    sass works

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,17 @@ import os
 from app.config import config
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from sassutils.wsgi import SassMiddleware
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='/static')
+app.wsgi_app = SassMiddleware(app.wsgi_app, {
+    'app': ('static/sass', 'static/css', 'static/css')
+})
+
 db = SQLAlchemy()
 
 def create_app(cfg):
     app.config.from_object(config.get(cfg))
     db.init_app(app)
+    
     return app
