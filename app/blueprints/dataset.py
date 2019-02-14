1ec837f8df0765890c9b8d32edf76b701a4f11e5
Liam <liamdalg99@gmail.com>
2019-02-14 00:33:15 +0000

    dataset download and renaming

--- app/blueprints/dataset.py
+++ app/blueprints/dataset.py
@@ -1,8 +1,9 @@
 """Dataset blueprint module for routes to modify datasets."""
-from flask import render_template, request, abort, escape
+from flask import render_template, request, abort, escape, send_file
 from flask.blueprints import Blueprint
 from app import models
 from app.models import db
+import io
 
 bp = Blueprint('dataset', __name__, url_prefix='/dataset')
 
@@ -16,7 +17,7 @@ def view(name):
 @bp.route('/upload', methods=['POST'])
 def upload():
     if 'csv_file' not in request.files or 'name' not in request.form:
-        abort(400)
+        abort(400, 'Invalid request arguments.\n\tcsv_file: <bytes>\n\tname: <str>')
     csv_bytes = request.files.get('csv_file').read()
     name = request.form.get('name')
     dataset = models.Dataset(name=name, data=csv_bytes)
@@ -27,10 +28,23 @@ def upload():
 
 @bp.route('/rename', methods=['POST'])
 def rename():
-    abort(400, 'Not implemented yet!')
-
+    if 'old_name' not in request.form or 'new_name' not in request.form:
+        abort(400, 'Invalid request arguments.\n\told_name: <str>\n\tnew_name: <str>')
+    old_name = request.form.get('old_name')
+    new_name = request.form.get('new_name')
+    models.Dataset.query.filter_by(name=old_name).update(dict(name=new_name))
+    db.session.commit()
+    return 'Success'
 
 @bp.route('/download')
 def download():
-    abort(400, 'Not implemented yet!')
+    if 'name' not in request.args:
+        abort(400, 'Invalid request arguments.\n\tname: <str>')
+    name = request.args.get('name')
+    dataset = models.Dataset.query.filter_by(name=name).first()
+    return send_file(io.BytesIO(dataset.data),
+                     as_attachment=True,
+                     attachment_filename=name + '.csv',
+                     mimetype='text/csv')
+
 
