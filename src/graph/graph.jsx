99b4fe3f6bda98aba05f26fa961a15f9064fc8b1
Nenivar <nenivar@gmail.com>
2019-02-20 13:35:16 +0000

    encapsulate graph tool

--- src/graph/graph.jsx
+++ src/graph/graph.jsx
@@ -1,74 +1,45 @@
 import React from "react";
 import { Graph } from 'react-d3-graph';
  
-// graph payload (with minimalist structure)
-const data = {
-    nodes: [{ id: 'Harry' }, { id: 'Sally' }, { id: 'Alice' }],
-    links: [{ source: 'Harry', target: 'Sally' }, { source: 'Harry', target: 'Alice' }]
-};
- 
-// the graph configuration, you only need to pass down properties
-// that you want to override, otherwise default ones will be used
-const myConfig = {
-    nodeHighlightBehavior: true,
-    node: {
-        color: 'lightgreen',
-        size: 120,
-        highlightStrokeColor: 'blue'
-    },
-    link: {
-        highlightColor: 'lightblue'
+// cheers https://github.com/danielcaldas/react-d3-graph/blob/master/sandbox/Sandbox.jsx
+export default class Tool extends React.Component {
+    constructor(props) {
+        super(props);
+
+        const data = {
+            nodes: [{ id: 'Harry' }, { id: 'Sally' }, { id: 'Alice' }],
+            links: [{ source: 'Harry', target: 'Sally' }, { source: 'Harry', target: 'Alice' }]
+        };
+
+        const config = {
+            nodeHighlightBehavior: true,
+            node: {
+                color: 'lightgreen',
+                size: 120,
+                highlightStrokeColor: 'blue'
+            },
+            link: {
+                highlightColor: 'lightblue'
+            }
+        };
+
+        this.state = {
+            config,
+            data
+        }
     }
-};
- 
-// graph event callbacks
-const onClickGraph = function() {
-    window.alert(`Clicked the graph background`);
-};
- 
-const onClickNode = function(nodeId) {
-    window.alert(`Clicked node ${nodeId}`);
-};
- 
-const onRightClickNode = function(event, nodeId) {
-    window.alert(`Right clicked node ${nodeId}`);
-};
- 
-const onMouseOverNode = function(nodeId) {
-    window.alert(`Mouse over node ${nodeId}`);
-};
- 
-const onMouseOutNode = function(nodeId) {
-    window.alert(`Mouse out node ${nodeId}`);
-};
- 
-const onClickLink = function(source, target) {
-    window.alert(`Clicked link between ${source} and ${target}`);
-};
- 
-const onRightClickLink = function(event, source, target) {
-    window.alert(`Right clicked link between ${source} and ${target}`);
-};
- 
-const onMouseOverLink = function(source, target) {
-    window.alert(`Mouse over in link between ${source} and ${target}`);
-};
- 
-const onMouseOutLink = function(source, target) {
-    window.alert(`Mouse out link between ${source} and ${target}`);
-};
- 
-<Graph
-    id="graph-id" // id is mandatory, if no id is defined rd3g will throw an error
-    data={data}
-    config={myConfig}
-    onClickNode={onClickNode}
-    onRightClickNode={onRightClickNode}
-    onClickGraph={onClickGraph}
-    onClickLink={onClickLink}
-    onRightClickLink={onRightClickLink}
-    onMouseOverNode={onMouseOverNode}
-    onMouseOutNode={onMouseOutNode}
-    onMouseOverLink={onMouseOverLink}
-    onMouseOutLink={onMouseOutLink}
-/>;
+
+    render() {
+        const graphProps = {
+            id: 'graph',
+            data: this.state.data,
+            config: this.state.config
+        }
+
+        return (
+            <div>
+                <Graph ref="graph" {...graphProps} />
+            </div>
+        );
+    }
+}
