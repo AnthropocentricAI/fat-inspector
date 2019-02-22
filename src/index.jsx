3e1160f583fe67f796de7f4faa389f1f1b64e057
Nenivar <nenivar@gmail.com>
2019-02-22 11:19:15 +0000

    ijfsapofjapofj

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Popup from './popup.jsx'
+import PopupForm from './popup_form.jsx'
+import PopupYesNo from './popup.jsx';
 
 //import './sass/main.scss'
 
 const App = () => (
     <div>
         <h2>hahahaha</h2>
-        <Popup></Popup>
+        {/* <Popup title="Title" desc="Description."></Popup>
+        <PopupForm></PopupForm> */}
+        <PopupYesNo title="Title" form={ PopupForm }>
+            <p>dsjaidjasio</p>
+        </PopupYesNo>
     </div>
 );
 
--- src/popup.jsx
+++ src/popup.jsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import PopupForm from './popup_form.jsx';
 
 export default class Popup extends React.Component {
     constructor(props) {
@@ -8,10 +9,19 @@ export default class Popup extends React.Component {
     render() {
         return (
             <div className="popup_cont">
-                <img className="popup_close" src="assets/gear.svg"></img>
-                <h1 className="popup_title">Title</h1>
-                <p>Words.</p>
+                <div>
+                    <img className="popup_close" src="assets/gear.svg"></img>
+                    <h2 className="popup_title">{ this.props.title }</h2>
+                </div>
+                <p className="popup_desc">{ this.props.desc }</p>
+                { this.props.children }
             </div>
         );
     }
-}
+};
+
+export function PopupYesNo(props) {
+    return <Popup title="test" desc="dsadoajdoias">
+        <p>aagjagj</p>
+    </Popup>;
+};
