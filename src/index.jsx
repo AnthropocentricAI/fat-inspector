e0585d7f3c46b3a02c37d87cec5b2bf8216cad35
Nenivar <nenivar@gmail.com>
2019-02-27 12:26:15 +0000

    actually merge. oops lol haha git status

--- src/index.jsx
+++ src/index.jsx
@@ -1,23 +1,3 @@
-<<<<<<< HEAD
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Popup from './popup.jsx'
-import PopupForm from './popup_form.jsx'
-import PopupYesNo from './popup.jsx';
-
-//import './sass/main.scss'
-
-const App = () => (
-    <div>
-        <h2>hahahaha</h2>
-        <Popup title="title" desc="desc">
-            <PopupForm></PopupForm>
-        </Popup>
-    </div>
-);
-
-ReactDOM.render(<App/>, document.getElementById('root'));
-=======
 import React from "react";
 import ReactDOM from "react-dom";
 import Topbar from "./topbar/Topbar.jsx";
@@ -32,4 +12,3 @@ const App = () => (
 );
 
 ReactDOM.render(<App />, document.getElementById("root"));
->>>>>>> 0648753d12a3ba6ab14ab76d7ed50c32dd59f16b
--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,6 @@
 const path = require("path");
 
 module.exports = {
-<<<<<<< HEAD
-    mode: 'production',
-    entry: './src/index.jsx',
-    output: {
-        path: path.resolve(__dirname, 'app/static/js'),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [{
-            test: /\.jsx$/,
-            exclude: /node_modules/,
-            loader: 'babel-loader'
-        },
-        {
-            test: /\.scss$/,
-            use: [
-                "style-loader",
-                "css-loader",
-                "sass-loader"
-            ]
-        }]
-    },
-    optimization: {
-        minimize: true,
-    }
-}
-=======
   mode: "production",
   entry: "./src/index.jsx",
   output: {
@@ -47,4 +20,3 @@ module.exports = {
     minimize: true
   }
 };
->>>>>>> 0648753d12a3ba6ab14ab76d7ed50c32dd59f16b
