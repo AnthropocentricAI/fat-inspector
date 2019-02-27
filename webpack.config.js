acccc7bf51cb6fbdf8c6cb597503d2ed05a0fe9e
liamdalg <liamdalg99@gmail.com>
2019-02-27 11:38:06 +0000

    SASS works!

--- webpack.config.js
+++ webpack.config.js
@@ -2,25 +2,41 @@ const path = require('path');
 
 module.exports = {
     mode: 'production',
-    entry: './src/index.jsx',
+    entry: ['./src/index.jsx', './src/sass/main.scss'],
     output: {
-        path: path.resolve(__dirname, 'app/static/js'),
-        filename: 'bundle.js'
+        path: path.resolve(__dirname, 'app/static/'),
+        filename: 'js/bundle.js'
     },
     module: {
-        rules: [{
-            test: /\.jsx$/,
-            exclude: /node_modules/,
-            loader: 'babel-loader'
-        },
-        {
-            test: /\.scss$/,
-            use: [
-                "style-loader",
-                "css-loader",
-                "sass-loader"
-            ]
-        }]
+        rules: [
+            {
+                test: /\.jsx$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: 'css/[name].css',
+                        }
+                    },
+                    {
+                        loader: 'extract-loader'
+                    },
+                    {
+                        loader: 'css-loader?-url'
+                    },
+                    {
+                        loader: 'postcss-loader'
+                    },
+                    {
+                        loader: 'sass-loader'
+                    }
+                ]
+            }]
     },
     optimization: {
         minimize: true,
