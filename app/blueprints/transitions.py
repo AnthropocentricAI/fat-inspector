3c132c0c942c47a61562f78131485fa328a39d83
Gregg Nurick <greggkn872@gmail.com>
2019-04-26 17:36:51 +0100

    my computer broke

--- app/blueprints/transitions.py
+++ app/blueprints/transitions.py
@@ -14,12 +14,20 @@ from app import utilities as util
 from app.exceptions import APIArgumentError
 
 from app.tree import load_tree
+from app.blueprints.graph import fetch
+
+import uuid
 
 bp = Blueprint('transition', __name__, url_prefix='/transition')
 
 @bp.route('<graphName>/<nodeID>/model')
 def findModel(graphName, nodeID):
-    path = 'idk'
-    tree = load_tree(path)
+    file_path = os.path.join(current_app.config['ASSETS_DIR'], 'pickles', graphName)
+    tree = load_tree(file_path)
     
-    node = tree.node_of(nodeId).
+    graph_id = tree.node_of(nodeId).graph_id_model
+    if not graph_id:
+        graph_id = uuid.uuid4()
+        tree.node_of(nodeId).graph_id_model = graph_id
+        save_tree(tree, file_path)
+    fetch(graph_id)
